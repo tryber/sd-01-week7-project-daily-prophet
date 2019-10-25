9eed332e09cf0416fe31c8ff493da1d47d69c3c3
antoniopaolillo <antonio_francisco_p@live.com>
2019-10-25 19:47:50 -0300

    finalizando dia 25/10

---
+++ antoniopaolillo/script.js
@@ -0,0 +1,11 @@
+// function aumentandoTamanhoDosArtigos() {
+//     let artigos = document.getElementsByTagName("article");
+
+//     for (let i = 0; i < artigos.length; i++) {
+//         let artigo = document.getElementsByTagName("article")[i];
+//         artigo.addEventListener("click", function () {
+//             artigo.classList.add("aumentarTamanhoArtigo");
+//         })
+//     }
+// }
+// aumentandoTamanhoDosArtigos();
