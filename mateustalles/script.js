232e97ffd778736212be9ac38db61c728f7ea95c
Mateus Talles <mateustalles@gmail.com>
2019-10-25 17:37:41 -0300

    Animação da coruja no topo da tela.

---
+++ mateustalles/script.js
@@ -0,0 +1,6 @@
+let owlImage = document.querySelector(".owl");
+window.addEventListener("load", function() {
+    setTimeout(function() {
+      owlImage.className="owl animation-appears";
+    }, 3000);
+});
