2fa02f16494d50fb863ea5a52d3a7cd91d120e4f
DougFunny <douglaseduardo220483@gmail.com>
2019-10-27 00:15:12 -0300

    update script
    Colocando a function da vassoura

---
+++ dougfunny/script.js
@@ -0,0 +1,5 @@
+function animarVassoura() {
+    let vassoura = document.querySelector(".vassoura")
+    vassoura.style.animation = "vassourinha 5s infinite "
+    vassoura.style.transform = "translateX(50px) scaleY(2) skewY(20deg)"
+}
