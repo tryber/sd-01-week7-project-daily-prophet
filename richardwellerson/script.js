9ba641b7e3c39522a7f2e1b363d36596f27c6979
Richard Wellerson <richardwellerson@gmail.com>
2019-10-27 15:56:09 -0300

    Adicionada funçao de clique na vassoura. Agora, terminar estilizaçao para JS.

--- richardwellerson/script.js
+++ richardwellerson/script.js
@@ -1,7 +1,7 @@
 function clickNimbus () {
     let selectNimbus = document.querySelector(".nimbus")
     selectNimbus.addEventListener("click", function(){
-        
+        selectNimbus.style.animation = "nimbus 5s infinite";
     })
 }
 clickNimbus ()
