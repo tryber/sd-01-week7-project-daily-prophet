4547b2505c20e31b8b1184eea16b6751de08dc12
Douglas Eduardo <douglaseduardo220483@gmail.com>
2019-10-27 11:06:22 -0300

    Colocando espaços

--- dougfunny/script.js
+++ dougfunny/script.js
@@ -3,14 +3,15 @@ function animarVassoura() {
     vassoura.style.animation = "vassourinha 5s infinite "
     vassoura.style.transform = "translateX(50px) scaleY(2) skewY(20deg)"
 }
+
 function larguraDaTela() {
     let largura = window.outerWidth
     let cabecalho = document.getElementById("cabecalho")
     let vassoura = document.querySelector(".vassoura")
     if (largura < 760) {
         cabecalho.src = "imagem/dailymobile.png"
-    }else {
-        cabecalho.src="imagem/profetadiario.png"
+    } else {
+        cabecalho.src = "imagem/profetadiario.png"
     }
-    
-}  
+
+}
