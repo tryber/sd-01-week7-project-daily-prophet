eadd78e79d4540f88015f9ff81a12283af649045
Douglas Eduardo <douglaseduardo220483@gmail.com>
2019-10-28 09:16:27 -0300

    detalhes da varinha, espaçamento

--- dougfunny/script.js
+++ dougfunny/script.js
@@ -1,6 +1,6 @@
 function animarVassoura() {
     let vassoura = document.querySelector(".vassoura")
-    vassoura.style.animation = "vassourinha 5s infinite "
+    vassoura.style.animation = "vassourinha 5s infinite forwards"
     vassoura.style.transform = "translateX(50px) scaleY(2) skewY(20deg)"
 }
 
