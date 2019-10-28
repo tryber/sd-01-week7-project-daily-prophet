7d8ff40cbdc798fa9dc2669629a7de9b4d3e0bc2
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-10-28 11:05:36 -0300

    Projeto todo estruturado

--- project/script.js
+++ project/script.js
@@ -1,5 +1,15 @@
 let animaçaoVassoura = document.getElementById('nimbus');
 
-animaçaoVassoura.addEventListener('click', function(){
+animaçaoVassoura.addEventListener('click', function() {
     animaçaoVassoura.className = 'nimbus'
+})
+
+let artigo = document.getElementsByTagName('article')[0];
+artigo.addEventListener('click', function() {
+    if (artigo.classList.contains('artigo')) {
+        artigo.classList.remove('artigo');
+    }
+    else {
+    artigo.className = "artigo"
+    }
 })
