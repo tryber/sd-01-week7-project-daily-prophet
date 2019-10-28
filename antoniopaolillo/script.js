4092fd161250520ad00394941565f2acc00b6ea7
antoniopaolillo <antonio_francisco_p@live.com>
2019-10-27 22:41:30 -0300

    adicionando versão mobile landscape

--- antoniopaolillo/script.js
+++ antoniopaolillo/script.js
@@ -1,11 +1,25 @@
-// function aumentandoTamanhoDosArtigos() {
-//     let artigos = document.getElementsByTagName("article");
+function aumentandoTamanhoDosArtigos() {
+    let artigos = document.getElementsByTagName("article");
 
-//     for (let i = 0; i < artigos.length; i++) {
-//         let artigo = document.getElementsByTagName("article")[i];
-//         artigo.addEventListener("click", function () {
-//             artigo.classList.add("aumentarTamanhoArtigo");
-//         })
-//     }
-// }
-// aumentandoTamanhoDosArtigos();
+    for (let i = 0; i < artigos.length; i++) {
+        let artigo = document.getElementsByTagName("article")[i];
+        artigo.addEventListener("click", function () {
+            artigo.classList.add("aumentarTamanhoArtigo");
+            setTimeout(function () {
+                artigo.classList.remove("aumentarTamanhoArtigo");
+            }, 4000);
+        })
+    }
+}
+aumentandoTamanhoDosArtigos();
+
+function ativarAnuncio() {
+    let vassoura = document.getElementById("vassouraAnuncio");
+    vassoura.addEventListener("click", function () {
+        vassoura.classList.add("vassouraAnuncioAdicionar");
+        setTimeout(function () {
+            vassoura.classList.remove("vassouraAnuncioAdicionar");
+        }, 10000);
+    })
+}
+ativarAnuncio();
