f2c3c486505e82c291960854a738af9fb843c88f
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-10-31 03:07:42 -0300

    Aside

--- bolivar/script.js
+++ bolivar/script.js
@@ -4,10 +4,12 @@ window.onload = function() {
     changeWordSize(searchByQuerySelector("#article1-title"), searchByQuerySelector("#section-one"), 0.03);
     changeWordSize(searchByQuerySelector(".news-title"), searchByQuerySelector("#section-two"), 0.1);
     changeWordSize(searchByQuerySelector(".news-text"), searchByQuerySelector("#section-two"), 0.013);
+    changeWordSize(searchByQuerySelector(".aside-title"), searchByQuerySelector("#aside-section"), 0.02);
     changeHeight(searchByQuerySelector("#florest-img"), searchByQuerySelector("#section-two"), 0.15);
     changeHeight(searchByQuerySelector("#tree-img"), searchByQuerySelector("#section-two"), 0.1);
-    changeHeight(searchByQuerySelector(".star-img"), searchByQuerySelector("#section-one"), 0.2);
-    changeHeight(searchByQuerySelector(".wicca-img"), searchByQuerySelector("#section-one"), 0.7);
+    changeHeight(searchByQuerySelector(".star-img"), searchByQuerySelector("#section-one"), 0.03);
+    changeHeight(searchByQuerySelector(".wicca-img"), searchByQuerySelector("#section-one"), 0.08);
+    changeHeight(searchByQuerySelector("#nimbus-img"), searchByQuerySelector("#aside-section"), 0.08);
 }
 
 window.onresize = function() {
@@ -16,10 +18,12 @@ window.onresize = function() {
     changeWordSize(searchByQuerySelector("#article1-title"), searchByQuerySelector("#section-one"), 0.03);
     changeWordSize(searchByQuerySelector(".news-title"), searchByQuerySelector("#section-two"), 0.1);
     changeWordSize(searchByQuerySelector(".news-text"), searchByQuerySelector("#section-two"), 0.013);
+    changeWordSize(searchByQuerySelector(".aside-title"), searchByQuerySelector("#aside-section"), 0.02);
     changeHeight(searchByQuerySelector("#florest-img"), searchByQuerySelector("#section-two"), 0.15);
     changeHeight(searchByQuerySelector("#tree-img"), searchByQuerySelector("#section-two"), 0.1);
-    changeHeight(searchByQuerySelector(".star-img"), searchByQuerySelector("#section-one"), 0.2);
-    changeHeight(searchByQuerySelector(".wicca-img"), searchByQuerySelector("#section-one"), 0.7);
+    changeHeight(searchByQuerySelector(".star-img"), searchByQuerySelector("#section-one"), 0.03);
+    changeHeight(searchByQuerySelector(".wicca-img"), searchByQuerySelector("#section-one"), 0.08);
+    changeHeight(searchByQuerySelector("#nimbus-img"), searchByQuerySelector("#aside-section"), 0.08);
 }
 
 
@@ -36,6 +40,6 @@ function changeWordSize(element_identifier, section_identifier, relative_size) {
 
 function changeHeight(element_identifier, section_identifier, relative_size) {
     for (i = 0; i < element_identifier.length; i++) {
-        element_identifier[i].style.height = `${section_identifier[0].clientHeight*relative_size}px`;
+        element_identifier[i].style.height = `${section_identifier[0].clientWidth*relative_size}px`;
     }
 }
