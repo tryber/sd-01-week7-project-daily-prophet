3d1f3bf248332e87ede4eb1e3fd06e3cd14ea1c1
Richard Wellerson <richardwellerson@gmail.com>
2019-10-30 22:40:01 -0300

    Melhorado js do projeto, retirando a adiçao de 'animation', e inserindo 'animation-play-state'

--- richardwellerson/script.js
+++ richardwellerson/script.js
@@ -1,17 +1,16 @@
+let selectNimbus = document.querySelector(".nimbus")
+
 function clickNimbus () {
-    let selectNimbus = document.querySelector(".nimbus")
     selectNimbus.addEventListener("click", function(){
-        selectNimbus.style.animation = "nimbus 5s infinite";
         selectNimbus.style.animationPlayState = "running";
     })
 }
-clickNimbus ()
 
 function pausedNimbus () {
-    let selectNimbus = document.querySelector(".nimbus")
     selectNimbus.addEventListener("dblclick", function(){
         selectNimbus.style.animationPlayState = "paused";
     })
 }
+
 clickNimbus ()
 pausedNimbus ()
