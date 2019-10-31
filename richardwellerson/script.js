51ed5c08b4e80d8298398e9d564278a2521535cb
Richard Wellerson <richardwellerson@gmail.com>
2019-10-31 18:22:48 -0300

    Colocado espaço entre a declaraçao da function e a chave

--- richardwellerson/script.js
+++ richardwellerson/script.js
@@ -1,14 +1,14 @@
 (function () {
     const selectNimbus = document.querySelector(".nimbus")
     const paragraphToGrow = document.querySelectorAll(".grow-paragraph")
-    selectNimbus.addEventListener("click", function(){
+    selectNimbus.addEventListener("click", function() {
         selectNimbus.style.animationPlayState = "running";
     })
-    selectNimbus.addEventListener("dblclick", function(){
+    selectNimbus.addEventListener("dblclick", function() {
         selectNimbus.style.animationPlayState = "paused";
     })
     for(let i = 0; i < paragraphToGrow.length; i++) {
-        paragraphToGrow[i].addEventListener('click', function(){
+        paragraphToGrow[i].addEventListener('click', function() {
             paragraphToGrow[i].style.animationPlayState = "running";
         })
     }
