c181e59d4b475d5b09a89b4345493efb387c64b5
Richard Wellerson <richardwellerson@gmail.com>
2019-10-31 18:27:06 -0300

    Alterado js para usar arrow functions

--- richardwellerson/script.js
+++ richardwellerson/script.js
@@ -1,15 +1,9 @@
 (function () {
     const selectNimbus = document.querySelector(".nimbus")
     const paragraphToGrow = document.querySelectorAll(".grow-paragraph")
-    selectNimbus.addEventListener("click", function() {
-        selectNimbus.style.animationPlayState = "running";
-    })
-    selectNimbus.addEventListener("dblclick", function() {
-        selectNimbus.style.animationPlayState = "paused";
-    })
+    selectNimbus.addEventListener("click", () => selectNimbus.style.animationPlayState = "running")
+    selectNimbus.addEventListener("dblclick", () => selectNimbus.style.animationPlayState = "paused")
     for(let i = 0; i < paragraphToGrow.length; i++) {
-        paragraphToGrow[i].addEventListener('click', function() {
-            paragraphToGrow[i].style.animationPlayState = "running";
-        })
+        paragraphToGrow[i].addEventListener('click', () => paragraphToGrow[i].style.animationPlayState = "running")
     }
 }())
