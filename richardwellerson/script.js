ac7bd03b0c84c8a3d6ecd84227a1eebdff9e2ff6
Richard Wellerson <richardwellerson@gmail.com>
2019-10-31 17:49:09 -0300

    Alterando js para funcoes anonimas

--- richardwellerson/script.js
+++ richardwellerson/script.js
@@ -2,26 +2,23 @@ const selectNimbus = document.querySelector(".nimbus")
 const paragraphToGrow = document.querySelectorAll(".grow-paragraph")
 
 
-function clickNimbus () {
+(function() {
     selectNimbus.addEventListener("click", function(){
         selectNimbus.style.animationPlayState = "running";
     })
-}
+}());
 
-function pausedNimbus () {
+(function() {
     selectNimbus.addEventListener("dblclick", function(){
         selectNimbus.style.animationPlayState = "paused";
     })
-}
+}());
 
-function growParagraph () {
+(function() {
     for(let i = 0; i < paragraphToGrow.length; i++) {
         paragraphToGrow[i].addEventListener('click', function(){
             paragraphToGrow[i].style.animationPlayState = "running";
         })
     }
-}
+}());
 
-clickNimbus ()
-pausedNimbus ()
-growParagraph ()
