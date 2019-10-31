998dcea48b9989e8990624ef795c2b1ae8ca2b71
Richard Wellerson <richardwellerson@gmail.com>
2019-10-30 22:33:44 -0300

    Adicionando funçao de pausar nimbus atraves do dblclick

--- richardwellerson/script.js
+++ richardwellerson/script.js
@@ -2,6 +2,16 @@ function clickNimbus () {
     let selectNimbus = document.querySelector(".nimbus")
     selectNimbus.addEventListener("click", function(){
         selectNimbus.style.animation = "nimbus 5s infinite";
+        selectNimbus.style.animationPlayState = "running";
     })
 }
 clickNimbus ()
+
+function pausedNimbus () {
+    let selectNimbus = document.querySelector(".nimbus")
+    selectNimbus.addEventListener("dblclick", function(){
+        selectNimbus.style.animationPlayState = "paused";
+    })
+}
+clickNimbus ()
+pausedNimbus ()
