44473b32fb97b8cb1b2bb585524e5bb7adcec888
Mateus Talles <mateustalles@gmail.com>
2019-11-01 09:30:27 -0300

    Código refatorado e CSS "alfabetizado"

--- mateustalles/script.js
+++ mateustalles/script.js
@@ -5,42 +5,44 @@ let broom = document.querySelector(".broom");
 let articles = document.getElementsByTagName("article");
 
 function putBroomStyle() {
-    this.style = "animation: broom 5s forwards ease-in;";
-    this.classList.remove("halt-animation");
+    if ( window.innerWidth >= 760 ) {
+        broom.addEventListener("click", putBroomStyleBig )
+    }
+    else {
+        broom.addEventListener("click", putBroomStyleSmall )
+    }
+}
+
+function putBroomStyleBig() {
+    broom.style = "animation: broom 5s forwards ease-in;";
+    broom.classList.remove("halt-animation");
 };
 
 function putBroomStyleSmall () {
-    this.style = "animation: broom 5s steps(10, end) forwards"
-    this.classList.remove("halt-animation")
+    broom.style = "animation: broom 5s steps(10, end) forwards"
+    broom.classList.remove("halt-animation")
 }
 
 function animate() {
+    putBroomStyle()
     for (let article of articles) {
         article.className = ""
-    }
     if ( window.innerWidth >= 760 ) {
-        broom.addEventListener("click", putBroomStyle);
-        for (article of articles) {
-            article.addEventListener("click", function(e, size = "big") {
-                switch (this.className) {
-                    case "" :
-                        this.className = `article-animation-${size}`
-                        break
-                    case `article-animation-${size}` :
-                        this.className = "halt-animation"
-                        break
-                    case "halt-animation" :
-                        this.className = `article-animation-${size}`
-                        break
-                    }
+        article.addEventListener("click", function(e, size = "big") {
+            switch (this.className) {
+                case "" :
+                    this.className = `article-animation-${size}`
+                    break
+                case `article-animation-${size}` :
+                    this.className = "halt-animation"
+                    break
+                case "halt-animation" :
+                    this.className = `article-animation-${size}`
+                    break
+                }
             })
         }    
-    }
     else if (window.innerWidth < 760) {
-        broom.addEventListener("click", function() {
-            putBroomStyleSmall
-        });
-        for (article of articles) {
         article.addEventListener("click", function(e, size = "small") {
             switch (this.className) {
                 case "" :
