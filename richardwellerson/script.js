0d08976b6ffce9c6d3648ed62cb8c4baf0fb40fd
richardwellerson <richardwellerson@gmail.com>
2019-12-27 20:50:04 -0300

    Formataçao CC

--- richardwellerson/script.js
+++ richardwellerson/script.js
@@ -1,9 +1,9 @@
 (function () {
-    const selectNimbus = document.querySelector(".nimbus")
-    const paragraphToGrow = document.querySelectorAll(".grow-paragraph")
-    selectNimbus.addEventListener("click", () => selectNimbus.style.animationPlayState = "running")
-    selectNimbus.addEventListener("dblclick", () => selectNimbus.style.animationPlayState = "paused")
+    const selectNimbus = document.querySelector(".nimbus");
+    const paragraphToGrow = document.querySelectorAll(".grow-paragraph");
+    selectNimbus.addEventListener("click", () => selectNimbus.style.animationPlayState = "running");
+    selectNimbus.addEventListener("dblclick", () => selectNimbus.style.animationPlayState = "paused");
     for(let paragraph = 0; paragraph < paragraphToGrow.length; paragraph++) {
-        paragraphToGrow[paragraph].addEventListener('click', () => paragraphToGrow[paragraph].style.animationPlayState = "running")
-    }
+        paragraphToGrow[paragraph].addEventListener('click', () => paragraphToGrow[paragraph].style.animationPlayState = "running");
+    };
 }())
